cb39c12e2215709a0af7901e15eadac0d3d56de5
chen <chen@163.com>
2024-09-06 16:58:05 +0800

    添加pcap相关接口函数

---
+++ net/plat/netif_pcap.h
@@ -0,0 +1,18 @@
+/*
+ *  @brief: pcap虚拟网络接口
+ */
+
+#ifndef _NETIF_PCAP_H_
+#define _NETIF_PCAP_H_
+
+#include "netif.h"
+
+// pcap网络接口的驱动数据
+typedef struct _pcap_data_t {
+    const char* ip;                    // 使用的网卡
+    const uint8_t* hwaddr;             // 网卡的mac地址
+}pcap_data_t;
+
+extern const netif_ops_t pcap_ops;
+
+#endif
